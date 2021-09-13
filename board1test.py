0dc38d9e6a95ed939a322793f2c4b76502a8ff53
Brian Windrim <bwindrim@gmail.com>
2021-09-13 17:30:44 +0100

    Adding unfinished, non-working, version of board1test.py

---
+++ board1test.py
@@ -0,0 +1,177 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO")
+    
+assert(GPIO.getmode() == None)
+
+GPIO.setmode(GPIO.BOARD); # use Pi connector pin numbering
+
+# data bus (input/output)
+D0 = 11  # GP17
+D1 = 12  # GP18
+D2 = 13  # GP27
+D3 = 15  # GP22
+D4 = 16  # GP23
+D5 = 19  # GP10
+D6 = 21  # GP09
+D7 = 23  # GP11
+data_bus = [D7, D6, D5, D4, D3, D2, D1, D0]
+GPIO.setup(data_bus, GPIO.IN)
+
+# chip selects (output, active-low, pull-up)
+CS0 = 26 # GP07
+CS1 = 29 # GP05
+CS2 = 31 # GP06
+chip_selects = [CS0, CS1, CS2]
+GPIO.setup(chip_selects, GPIO.OUT)
+GPIO.output(chip_selects, GPIO.HIGH)
+CS_portA = CS1
+CS_portB = CS0
+CS_handshake = CS2
+
+# handshakes (active-low)
+CA1 = 32 # GP12 - output
+CA2 = 33 # GP13 - input
+CB1 = 36 # GP16 - output
+CB2 = 38 # GP20 - input
+PortA_DATA_READY = CA1
+PortA_DATA_TAKEN = CA2
+PortB_DATA_TAKEN = CB1
+PortB_DATA_READY = CB2 
+GPIO.setup(PortA_DATA_READY, GPIO.OUT) # "data ready" output
+GPIO.setup(PortA_DATA_TAKEN, GPIO.IN)  # "data taken" input
+GPIO.setup(PortB_DATA_TAKEN, GPIO.OUT) # "data taken" output
+GPIO.setup(PortB_DATA_READY, GPIO.IN)  # "data ready" input
+GPIO.output([PortA_DATA_READY, PortB_DATA_TAKEN], GPIO.HIGH) # clear handshakes
+
+# 6809 processor control (output, active-high)
+RST = 24 # GP08
+NMI = 22 # GP25
+proc_control = [RST, NMI]
+GPIO.setup(proc_control, GPIO.OUT)
+GPIO.output(proc_control, GPIO.LOW)
+
+# setup for output to port A
+#GPIO.output(CS_handshake, GPIO.LOW)       # enable IC2, to pass handshake signals to/from target
+GPIO.output(CS_portB, GPIO.HIGH) # ensure port B is deselected, before we select port A
+GPIO.setup(data_bus, GPIO.OUT)   # set data bus for output
+GPIO.output(CS_portA, GPIO.LOW)  # enable IC1, to pass data from the bus to port A
+
+def bus_read():
+    "Read the 8 bits of the data bus into a list"
+    B0 = GPIO.input(D0)
+    B1 = GPIO.input(D1)
+    B2 = GPIO.input(D2)
+    B3 = GPIO.input(D3)
+    B4 = GPIO.input(D4)
+    B5 = GPIO.input(D5)
+    B6 = GPIO.input(D6)
+    B7 = GPIO.input(D7)
+    
+    return [B7, B6, B5, B4, B3, B2, B1, B0]
+
+
+def send_byte(int8):
+    "write a byte to Port A, with handshake, and read back from port B"
+    # setup for output to port A
+    assert(int8 < 256)
+    GPIO.output(CS_portB, GPIO.HIGH) # ensure port B is deselected, before we select port A
+    GPIO.setup(data_bus, GPIO.OUT)   # set data bus for output
+    GPIO.output(CS_portA, GPIO.LOW)  # enable IC1, to pass data from the bus to port A
+#    GPIO.output(CS_handshake, GPIO.LOW)       # enable IC2, to pass handshake signals to/from target
+
+    output = [int(x) for x in '{:08b}'.format(int8)] # pythonically unpack byte to list of bits
+    GPIO.output(data_bus, output)
+    GPIO.output(PortA_DATA_READY, GPIO.LOW)   # signal data ready
+    GPIO.output(PortA_DATA_READY, GPIO.HIGH)  # clear data ready
+        
+    # read back
+    # setup for input from port B
+    GPIO.output(CS_portA, GPIO.HIGH) # ensure port A is deselected before we select port B
+    GPIO.setup(data_bus, GPIO.IN)   # set data bus for input
+    GPIO.output(CS_portB, GPIO.LOW)  # enable IC0, to pass data from port B to the bus
+    
+    # read data from bus, compare output and input
+    input = bus_read()
+    if input != output:
+        print("output = ", output, "input = ", input)
+        
+    GPIO.output(PortB_DATA_TAKEN, GPIO.LOW) # signal data taken
+    GPIO.output(PortB_DATA_TAKEN, GPIO.HIGH) # clear data taken
+                
+    GPIO.output(CS_portB, GPIO.HIGH) # disable IC0                
+
+    return int8
+
+def send_word(word):
+    send_byte(word >> 8)
+    send_byte(word & 0xFF)
+    return word
+
+def get_byte():
+    input = bus_read()
+    GPIO.output(PortB_DATA_TAKEN, GPIO.LOW) # signal data taken
+    GPIO.output(PortB_DATA_TAKEN, GPIO.HIGH) # clear data taken
+    
+    out = 0
+    for bit in input:
+        out = (out << 1) | bit
+        
+    return out
+
+def get_byte2():
+    input = bus_read()
+    GPIO.output(PortB_DATA_TAKEN, GPIO.LOW) # signal data taken
+    GPIO.output(PortB_DATA_TAKEN, GPIO.HIGH) # clear data taken
+    
+    out = 0
+    for bit in input:
+        out = (out << 1) | bit
+        
+    return out
+
+def listen():
+    print("Listening...")
+    # setup for input from port B
+    GPIO.output(CS_portA, GPIO.HIGH) # ensure port A is deselected before we select port B
+    GPIO.setup(data_bus, GPIO.IN)   # set data bus for input
+    GPIO.output(CS_portB, GPIO.LOW)  # enable IC0, to pass data from port B to the bus
+
+    GPIO.output(CS_handshake, GPIO.LOW)       # signal that we're entering the listen phase
+  
+    for i in range(10):
+        byte = get_byte2()
+        
+        if None != byte:
+            print ("byte = ", hex(byte))
+        
+    return
+
+with open ("newtick.ex9", 'rb') as f:
+    send_byte(0xAA)
+    
+    word = f.read(2)
+    load_addr = int.from_bytes(word, "big")
+    word = f.read(2)
+    length = int.from_bytes(word, "big")
+    print ("load address = ", hex(load_addr), "length = ", length);
+    send_word(load_addr)
+    send_word(length)
+
+    while length:
+        length -= 1
+        byte = f.read(1)
+        send_byte(int.from_bytes(byte, "big"))
+        
+    word = f.read(2)
+    exec_addr = int.from_bytes(word, "big")
+    print ("exec address = ", hex(exec_addr));
+    #GPIO.output(CS_handshake, GPIO.HIGH)
+    send_word(exec_addr)
+
+listen()
+print ("Done.")
+
+
+GPIO.cleanup()
